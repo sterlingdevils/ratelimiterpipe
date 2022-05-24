71f423e7e71745fc21d55d86e6b9a481348452f7
Adam Snyder <adam.snyder@caci.com>
2022-05-24 16:24:09 -0400

    added pipelining

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/sterlingdevils/ratelimiterpipe
 go 1.18
 
 require golang.org/x/time v0.0.0-20220411224347-583f2d630306
+
+require github.com/sterlingdevils/pipelines v0.0.1 // indirect
--- pkg/ratelimiterpipe/ratelimiterpipe.go
+++ pkg/ratelimiterpipe/ratelimiterpipe.go
@@ -3,6 +3,7 @@ package ratelimiterpipe
 import (
 	"context"
 
+	"github.com/sterlingdevils/pipelines/pkg/pipeline"
 	"golang.org/x/time/rate"
 )
 
@@ -22,6 +23,8 @@ type RateLimiterPipe[T Sizeable] struct {
 
 	inchan  chan T
 	outchan chan T
+
+	pl pipeline.Pipelineable[T]
 }
 
 // InChan
@@ -34,17 +37,27 @@ func (r *RateLimiterPipe[T]) OutChan() <-chan T {
 	return r.outchan
 }
 
+// PipelineChan returns a R/W channel that is used for pipelining
+func (r *RateLimiterPipe[T]) PipelineChan() chan T {
+	return r.outchan
+}
+
+func (r *RateLimiterPipe[_]) Close() {
+	// If we pipelined then call Close the input pipeline
+	if r.pl != nil {
+		r.pl.Close()
+	}
+
+	// Cancel our context
+	r.can()
+}
+
 func (r *RateLimiterPipe[_]) SetLimit(l rate.Limit) {
 	r.limit.SetLimit(l)
 }
 
 func (r *RateLimiterPipe[_]) SetBurst(n int) {
 	r.limit.SetBurst(n)
-}
-
-func (r *RateLimiterPipe[_]) Close() {
-	// Cancel our context
-	r.can()
 }
 
 func (r *RateLimiterPipe[_]) mainloop() {
@@ -64,16 +77,35 @@ func (r *RateLimiterPipe[_]) mainloop() {
 	}
 }
 
-func New[T Sizeable](rLimit rate.Limit, bLimit int) (*RateLimiterPipe[T], error) {
+func NewWithChannel[T Sizeable](rLimit rate.Limit, bLimit int, in chan T) (*RateLimiterPipe[T], error) {
 	con, cancel := context.WithCancel(context.Background())
 	r := RateLimiterPipe[T]{
 		limit:   rate.NewLimiter(rLimit, bLimit),
 		ctx:     con,
 		can:     cancel,
-		inchan:  make(chan T, CHANSIZE),
+		inchan:  in,
 		outchan: make(chan T, CHANSIZE)}
 
 	go r.mainloop()
 
 	return &r, nil
+}
+
+func NewWithPipeline[T Sizeable](rLimit rate.Limit, bLimit int, p pipeline.Pipelineable[T]) (*RateLimiterPipe[T], error) {
+	r, err := NewWithChannel(rLimit, bLimit, p.PipelineChan())
+	if err != nil {
+		return nil, err
+	}
+
+	r.pl = p
+	return r, nil
+}
+
+func New[T Sizeable](rLimit rate.Limit, bLimit int) (*RateLimiterPipe[T], error) {
+	r, err := NewWithChannel(rLimit, bLimit, make(chan T, CHANSIZE))
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
 }
